04e86b044cb1308a1170515ccabbd43c48a6db17
augusto <augusto_prudencio@hotmail.com>
2023-06-29 01:00:47 -0300

    added parquimetro image and value in reals

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -8,24 +8,34 @@ import QrcodeVue from 'qrcode.vue'
 
 
 const value = ref(null);
-const info = ref({dataIni: "", dataFin: "", placa: "", id: 37})
+const valor = ref(null);
+const info = ref({dataIni: null, dataFin: null, placa: "", id: 37})
 const createQRCode = (()  => {
     axios.post("http://localhost:8000/add", {dataIni: String(info.value.dataIni).replace('-', '/'), dataFin: String(info.value.dataFin).replace('-', '/'), placa: String(info.value.placa)}, {headers: {'content-type': 'application/x-www-form-urlencoded'}})
-        .then((res) => {info.value.id = res.data._id; value.value = "inicio: " + String(info.value.dataIni).replace('-', '/') + ",fim: " + String(info.value.dataFin).replace('-', '/') + ",placa: " + String(info.value.placa) + ",id: " + String(info.value.id);});
+        .then((res) => {
+          info.value.id = res.data._id; 
+          value.value = "inicio: " + String(info.value.dataIni).replace('-', '/') + ",fim: " + String(info.value.dataFin).replace('-', '/') + ",placa: " + String(info.value.placa) + ",id: " + String(info.value.id);
+          valor.value = Math.abs(info.value.dataFin - info.value.dataIni) / 36e5;
+        });
     
+})
+const changeValor = (() => {
+  if(info.value.dataIni && info.value.dataFin) valor.value = Math.abs(info.value.dataFin - info.value.dataIni) / 36e5;
 })
 
 </script>
 
 <template>
   <header>
+    <img alt="Parquimetro" class="logo" src="../assets/parquimetro-image.png" width="125" height="125" />
     <h1>Monte seu horário!</h1>
     <div class="wrapper">
-      <Calendar class="InputText" dateFormat="dd/mm/yy" autofocus type="text" v-model="info.dataIni" placeholder="Inicio" showTime hourFormat="24" showIcon v-on:keyup.enter="createQRCode" />
-      <Calendar type="text" dateFormat="dd/mm/yy" v-model="info.dataFin" placeholder="Fim" showTime hourFormat="24" showIcon v-on:keyup.enter="createQRCode" />
+      <Calendar class="InputText" dateFormat="dd/mm/yy" autofocus type="text" v-model="info.dataIni" placeholder="Inicio" showTime hourFormat="24" showIcon v-on:date-select="changeValor" v-on:keyup.enter="createQRCode" />
+      <Calendar type="text" dateFormat="dd/mm/yy" v-model="info.dataFin" placeholder="Fim" showTime hourFormat="24" showIcon v-on:date-select="changeValor" v-on:keyup.enter="createQRCode" />
       <InputText class="InputText" type="text" v-model="info.placa" placeholder="Placa" minlength="1" v-on:keyup.enter="createQRCode" />
       <Button label="Envia" v-on:click="createQRCode" />
     </div>
+    <h2 v-if="valor"> Horas: {{ valor }}, Valor total: R${{ valor * 1.5 }}</h2>
   </header>
   
   <main>
