9cc02a84c0c4fb64e577766514b08086b59aa3ed
Marcelo Custódio <custodios.marcelo@gmail.com>
2023-07-02 20:30:11 -0300

    pwa done

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,3 @@
-//const Html5QrcodeScanner = require("html5-qrcode-scanner");
-
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("sw.js").then(registration => {
         console.log("Service Worker Registered");
@@ -13,36 +11,39 @@ if ("serviceWorker" in navigator) {
 }
 
 const scanner = new Html5QrcodeScanner('reader', { 
-    // Scanner will be initialized in DOM inside element with id of 'reader'
     qrbox: {
         width: 250,
         height: 250,
-    },  // Sets dimensions of scanning box (set relative to reader element width)
-    fps: 20, // Frames per second to attempt a scan
+    },
+    fps: 20
 });
-
-console.log(scanner);
-
 scanner.render(success, error);
-// Starts scanner
 
 function success(result) {
 
+    var qrdata = JSON.parse(result);
+    console.log(qrdata);
+    var dataini = Date.parse(qrdata.inicio);
+    var datafim = Date.parse(qrdata.fim);
+    var datanow = Date.now();
+    var diff = datanow - datafim;
+    var valor = ((datafim - dataini)/36e5)*1.5;
+    var multa = 0;
+    
+    if (diff > 0) multa = (diff/36e5)*3;
+    valor += multa;
+
+    console.log(dataini, datafim, datanow, valor, multa);
+
     document.getElementById('result').innerHTML = `
-    <h2>Success!</h2>
-    <p><a href="${result}">${result}</a></p>
-    `;
-    // Prints result as a link inside result element
+        <p>${result}</p>
+        <p>Valor: R$ ${valor.toFixed(2)}</p>
+        <p>Multa: R$ ${multa.toFixed(2)}</p>`;
 
     scanner.clear();
-    // Clears scanning instance
-
-    document.getElementById('reader').remove();
-    // Removes reader element from DOM since no longer needed
-    
+    document.getElementById('reader').remove();    
 }
 
 function error(err) {
-    console.error(err);
-        // Prints any errors to the console
+    //console.error(err);
 }
